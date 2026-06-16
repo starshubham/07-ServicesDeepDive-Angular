8a8ca78064252940eaabf1af46588bfe956126bd
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-06-16 15:46:19 +0530

    Working with Services without using Signals

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,10 +7,15 @@ import { LoggingService } from '../logging.service';
 // })
 
 export class TasksService {
-    private tasks = signal<Task[]>([]);
+    // private tasks = signal<Task[]>([]);
+    private tasks: Task[] = [];
     private loggingService = inject(LoggingService);
 
-    allTasks = this.tasks.asReadonly();
+    // allTasks = this.tasks.asReadonly();
+
+    get allTasks() {
+        return [...this.tasks];
+    }
 
     addTask(taskData: { title: string; description: string }) {
         const newTask: Task = {
@@ -18,14 +23,17 @@ export class TasksService {
             id: Math.random().toString(),
             status: 'OPEN'
         };
-        this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+        // this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+        this.tasks = [...this.tasks, newTask];
         this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title);
     }
 
     updateTaskStatus(taskId: string, newStatus: TaskStatus) {
-        this.tasks.update((oldTasks) => oldTasks.map((task) =>
-            task.id === taskId ? { ...task, status: newStatus } : task
-    ));
+        // this.tasks.update((oldTasks) => oldTasks.map((task) =>
+        //     task.id === taskId ? { ...task, status: newStatus } : task
+        this.tasks = this.tasks.map((task) =>
+            task.id === taskId ? {...task, status: newStatus}: task
+    );
     this.loggingService.log('CHANGE TASK STATUS TO ' + newStatus);
     }
 }
